a9d3126e3229396f450fc1d61d715aa301af9c40
tincan1596 <sahilatwal2003@gmail.com>
2025-08-08 12:33:27 +0000

    run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Create and activate virtualenv if not exists
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+else
+    source venv/bin/activate
+fi
+
+# Run the PyQt6 app
+python3 app.py
