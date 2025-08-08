a57b20c4c07df8aca48b691f8915978c36fa0f81
tincan1596 <sahilatwal2003@gmail.com>
2025-08-08 13:03:05 +0000

    finalized ... maybe

--- run.bat
+++ run.bat
@@ -1,2 +1,8 @@
 @echo off
-wsl ./run.sh
+REM Get the directory of this .bat file (Windows path)
+set SCRIPT_DIR=%~dp0
+
+REM Call WSL directly with the converted path and execute run.sh
+wsl bash -c "cd \"$(wslpath '%SCRIPT_DIR%')\" && ./run.sh"
+
+pause
--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Create and activate virtualenv if not exists
 if [ ! -d "venv" ]; then
-    python3 -m venv venv
-    source venv/bin/activate
+    python3 -m venv .venv
+    source .venv/bin/activate
     pip install -r requirements.txt
 else
-    source venv/bin/activate
+    source .venv/bin/activate
 fi
 
 # Run the PyQt6 app
